92dd33bd5411de3ba7033bd5bd4dad3356b03d1c
anna <slukin1@yandex.ru>
2022-09-17 21:00:08 +0700

    Print

--- Task/Program.cs
+++ Task/Program.cs
@@ -14,3 +14,14 @@ string[] GetArray(int size)
     return array;
 }
 
+void PrintArray(object[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("] -> ");
+}
+
