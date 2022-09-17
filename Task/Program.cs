b3edc8f8edd155df02c7d0eee95f9402a3ea48e2
anna <slukin1@yandex.ru>
2022-09-17 20:29:41 +0700

    Created an array

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,16 @@
 ﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина, которых меньше или равна три символа.
 Console.Clear();
 
+Console.WriteLine("Введите любые четыре значения через пробел");
+
+string[] GetArray(int size)
+{
+    string[] array = new string[size];
+    string[] element = Console.ReadLine().Split(' ');
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = element[i];
+    }
+    return array;
+}
+
